96f7c5134ea59f6b60d1850ec6cca93ebdaa0eb4
Justus <der.infamous@googlemail.com>
2018-09-12 19:31:02 +0200

    Initial

---
+++ DapperTestManyToMany/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DapperTestManyToMany
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Chor> Chöre { get; set; } = new List<Chor>();
+    }
+}
